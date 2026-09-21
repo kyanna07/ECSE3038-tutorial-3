406a23e6d56039d91559ef36aa9af91c108d44c8
Kyanna Knight <knightkyanna@gmail.com>
2026-09-21 15:31:04 -0500

    task 6: create device

--- app.py
+++ app.py
@@ -49,3 +49,8 @@ def get_device(name: str):
 @app.get("/stats")
 def get_stats():
     return {"average_temperature": round(average_temp(readings), 2)}
+
+@app.post("/devices", status_code=201)
+def create_device(device: dict):
+    readings.append(device)
+    return device
